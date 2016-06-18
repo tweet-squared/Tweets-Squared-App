3c7f55ecea9f7f8c2128a7b1574b059aa4b14c29
Monica Mow <mow.monica@gmail.com>
2016-06-18 11:01:09 -0700

    Add a view for each topic when you click on a topic
    - added a new action to take you to individual topic
      > it will find topic by id
    - created view to show each individual topic when clicked
    - check to see if individual topic page works

--- app/actions.rb
+++ app/actions.rb
@@ -1,7 +1,13 @@
 # Homepage (Root path)
+# redirects to topics landing page
 get '/' do
   @topics = Topic.all.order(:topic)
   erb :'/topics/index'
+end
+
+get '/topics/:id' do 
+  @topic = Topic.find(params[:id])
+  erb :'topics/show'
 end
 
 
