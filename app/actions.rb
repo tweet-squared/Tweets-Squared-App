c8f275f57109ad4d13502b84497ecedd911640ad
Monica Mow <mow.monica@gmail.com>
2016-06-19 13:14:31 -0700

    Will this merge/rebase work??????

--- app/actions.rb
+++ app/actions.rb
@@ -19,7 +19,6 @@ get '/topics' do
   erb :'/topics/index.html'
 end
 
-<<<<<<< 7ff8c0ce666a9827094533e7e2b289982ef76434
 get '/verify/:id' do |id|
   tweet = Tweet.find(id)
   session[:times] = (session[:times] || 0) + 1
@@ -27,12 +26,4 @@ get '/verify/:id' do |id|
     session[:score] = (session[:score] || 0) + 1
   end
   redirect "/topics/#{session[:topic] || "all"}"
-=======
-get '/topics/all' do
-  erb :'/topics/all.html'
->>>>>>> Add materialize-type cards to /topics/all
-end
-
-get '/topics/:id' do
-  erb :'/topics/all.html'
 end
