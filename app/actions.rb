ae3ff9db28f60b0d95f30bedefc477575f323a0d
William Youngc <william@werl.me>
2016-06-18 22:25:22 -0400

    Create `verify/<id>` route
    `verify/<id>` checks if the score is correct, and adds to the `:score`
    of session. it also always adds one to the `:times` on session so that
    we know how many times the user has played.

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,12 @@
 get '/' do
   erb :index
 end
+
+get '/verify/:id' do
+  tweet = Tweet.find(id)
+  session[:times] = (session[:times] || 0) + 1
+  if tweet.twitter_handle.real_twitter_handle_id == nil
+    session[:score] = (session[:score] || 0) + 1
+  end
+  redirect '/topics/#{tweet.twitter_handle.topic.id}'
+end
