79f79d0fe7639af1581eebbe868c4d7e9ac6ab62
Monica Mow <mow.monica@gmail.com>
2016-06-19 13:01:15 -0700

    One more merge conflict fixed on actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -26,11 +26,4 @@ get '/verify/:id' do |id|
     session[:score] = (session[:score] || 0) + 1
   end
   redirect "/topics/#{session[:topic] || "all"}"
-<<<<<<< 0cbf5b54c2dcba508397b7947e86ca09ac322d22
-end
-
-get '/topics/:id' do
-  erb :'/topics/all.html'
-=======
->>>>>>> Fix the merge conflicts issues
 end
