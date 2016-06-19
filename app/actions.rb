8d28552c7cce17a0a0b6f9a5b125d146dd484371
William Youngc <william@werl.me>
2016-06-18 21:22:00 -0400

    Create `/topics/<id>` route shows the gambe
    This will load the erb file `topics/show.html.erb` from the `views`
    folder.
    
    This samples 2 tweets for the game from the selected topic (or all, if
    id is all)

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,13 @@
 get '/' do
   erb :index
 end
+
+get '/topics/:id' do |id|
+  @topics = Topic.all
+  unless id == "all"
+    @topics = [Topic.find(id)]
+  end
+  @real = @topics.sample.twitter_handles.where("real_twitter_handle_id is null").sample.tweets.sample
+  @fake = TwitterHandle.where("real_twitter_handle_id = ?", [@real.twitter_handle.id]).sample.tweets.sample
+  erb :'topics/show.html'
+end
